6ec9dd4ee31d97c0e4dfc0f808471acc0eb9c63f
Andrei Shevel <ash.developer@yandex.ru>
2015-08-18 17:33:46 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 'use strict';
 
-var mercury = require('./libs/mercury');
-
-mercury.Module = require('./libs/core/module');
-mercury.Router = require('./libs/core/router');
-mercury.Socket = require('./libs/core/socket');
-
-mercury.express = require('./libs/servers/express');
-mercury.io = require('./libs/servers/socket.io');
-
-module.exports = mercury;
+module.exports = require('./libs/mercury');
--- src/libs/mercury.js
+++ src/libs/mercury.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var path = require('path'),
-    _ = require('lodash-node');
+    _ = require('lodash-node'),
+    mercury;
 
 function Mercury() {
-
     this.mainPath = path.dirname(require.main.filename);
     this.config = _.extend(
         require('../mercury'),
@@ -12,7 +12,6 @@ function Mercury() {
     );
 
     this.modules = [];
-
 }
 
 Mercury.prototype.start = function () {
@@ -23,6 +22,12 @@ Mercury.prototype.addModule = function (module) {
     this.modules.push(module);
 };
 
-var mercury = new Mercury();
+mercury = new Mercury();
 
 module.exports = mercury;
+
+mercury.Module = require('./libs/core/module');
+mercury.Router = require('./libs/core/router');
+mercury.Socket = require('./libs/core/socket');
+mercury.express = require('./libs/servers/express');
+mercury.io = require('./libs/servers/socket.io');
