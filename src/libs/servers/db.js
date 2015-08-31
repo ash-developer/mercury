3907fefdea37778b883099652f6d573186a61595
ash <ash.developer@yandex.ru>
2015-08-31 22:09:37 +0300

    register repository

--- src/libs/servers/db.js
+++ src/libs/servers/db.js
@@ -1,9 +1,22 @@
 'use strict';
 
-var mercury = require('../mercury');
+var mercury = require('../mercury'),
+    repositories = {};
 
 function DB() {
     this.queryBuilder = require('knex')(mercury.config.db);
 }
+
+DB.prototype.registerRepository = function (name, repository) {
+    if (!repositories[name]) {
+        repositories[name] = new mercury.Repository();
+        repositories[name].table = name;
+    }
+
+    if (repository) {
+        repositories[name].table = repository.table;
+        util.inherits(repositories[name], repository);
+    }
+};
 
 module.exports = new DB();
