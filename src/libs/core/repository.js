1a2cf256acf1cceef40df1d59cacf01aebc17c8f
Andrei Shevel <ash.developer@yandex.ru>
2015-09-29 14:39:13 +0300

    conditions for repository list and remove

--- src/libs/core/repository.js
+++ src/libs/core/repository.js
@@ -5,8 +5,36 @@ var mercury = require('../mercury');
 function Repository() {
 }
 
-Repository.prototype.list = function (callback) {
-    mercury.db.queryBuilder(this.table).select().then(function (response) {
+Repository.prototype.list = function (conditions, callback) {
+    var self = this,
+        query = mercury.db.queryBuilder(this.table).select();
+
+    if (conditions) {
+        if (conditions.where) {
+            _.each(conditions.where, function (values, key) {
+                if (_.isArray(values)) {
+                    values.forEach(function (value) {
+                        query.where(self.table + '.' + key, value);
+                    });
+                } else {
+                    query.where(self.table + '.' + key, values);
+                }
+            });
+        }
+        if (conditions.whereNot) {
+            _.each(conditions.whereNot, function (values, key) {
+                if (_.isArray(values)) {
+                    values.forEach(function (value) {
+                        query.whereNot(self.table + '.' + key, value);
+                    });
+                } else {
+                    query.whereNot(self.table + '.' + key, values);
+                }
+            });
+        }
+    }
+
+    query.then(function (response) {
         callback(null, response);
     }).catch(callback);
 };
@@ -34,7 +62,35 @@ Repository.prototype.update = function (identifier, attributes, callback) {
 };
 
 Repository.prototype.remove = function (identifier, callback) {
-    mercury.db.queryBuilder(this.table).del().where(identifier).then(function (response) {
+    var self = this,
+        query = mercury.db.queryBuilder(this.table).del().where(identifier);
+
+    if (conditions) {
+        if (conditions.where) {
+            _.each(conditions.where, function (values, key) {
+                if (_.isArray(values)) {
+                    values.forEach(function (value) {
+                        query.where(self.table + '.' + key, value);
+                    });
+                } else {
+                    query.where(self.table + '.' + key, values);
+                }
+            });
+        }
+        if (conditions.whereNot) {
+            _.each(conditions.whereNot, function (values, key) {
+                if (_.isArray(values)) {
+                    values.forEach(function (value) {
+                        query.whereNot(self.table + '.' + key, value);
+                    });
+                } else {
+                    query.whereNot(self.table + '.' + key, values);
+                }
+            });
+        }
+    }
+
+    query.then(function (response) {
         callback(null, response);
     }).catch(callback);
 };
